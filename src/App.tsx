e7978ebe98dfe5c431c116bee15805a3996ce957
devmatias13 <saraviamatias678@gmail.com>
2026-04-27 10:30:52 -0300

    feat: añadido TopBar.tsx a la página de inicio de sesión

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import { LoginForm } from './features/auth';
+import { TopBar } from './components/common/TopBar';
 
 function App() {
   return (
-    <div className="w-full min-h-screen flex items-center justify-center bg-gray-50">
-      <LoginForm />
+    <div className="w-full min-h-screen flex flex-col bg-gray-50">
+      <TopBar />
+      <div className="flex-1 flex items-center justify-center p-4">
+        <LoginForm />
+      </div>
     </div>
   );
 }
