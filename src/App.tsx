a621f32df6d8917f31dca89e0de5711b3ca8c475
devmatias13 <saraviamatias678@gmail.com>
2026-04-27 11:21:07 -0300

    refactor: componente TopBar eliminado de App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
 import { LoginForm } from './features/auth';
-import { TopBar } from './components/common/TopBar';
 
 function App() {
   return (
-    <div className="w-full min-h-screen flex flex-col bg-gray-50">
-      <TopBar />
-      <div className="flex-1 flex items-center justify-center p-4">
-        <LoginForm />
-      </div>
+    <div className="w-full min-h-screen flex items-center justify-center bg-gray-50 p-4">
+      <LoginForm />
     </div>
   );
 }
